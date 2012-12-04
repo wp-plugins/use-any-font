12408cc5bbec4c46401f2fbe618fd211b6496f07
dnesscarkey <dnesscarkey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-04 03:16:22 +0000

    1.1 Release. (Error Fixes)
    git-svn-id: https://plugins.svn.wordpress.org/use-any-font/tags/1.1@633796 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/uaf_font_upload.php
@@ -0,0 +1,206 @@
+<?php
+if ($_POST['submit-uaf-font']){	
+	$uaf_api_key		= get_option('uaf_api_key');
+	$font_file_name 	= $_FILES['font_file']['name'];
+	$font_file_details 	= pathinfo($_FILES['font_file']['name']);
+	$file_extension		= strtolower($font_file_details['extension']);	
+	$upload_dir 		= wp_upload_dir();	
+	//if ($file_extension == 'woff'):
+		//$newFileName		= date('ymdhis').$_FILES['font_file']['name'];
+		//$newFilePath		= $upload_dir['path'].'/'.$newFileName;
+		//$newFileUrl			= $upload_dir['url'].'/'.$newFileName;
+		//move_uploaded_file($_FILES['font_file']['tmp_name'],$newFilePath);
+	//else:
+		$fontNameToStore 		= date('ymdhis').str_replace(' ','_',$font_file_details['filename']);
+		$fontNameToStoreWithUrl = $upload_dir['url'].'/'.$fontNameToStore;
+		//CONVERT TO WOFF
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_VERBOSE, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: multipart/form-data"));
+		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux i686; rv:6.0) Gecko/20100101 Firefox/6.0Mozilla/4.0 (compatible;)");
+		curl_setopt($ch, CURLOPT_URL, 'http://dineshkarki.com.np/font-convertor/woff.php');
+		curl_setopt($ch, CURLOPT_POST, true);
+		$post = array(
+			'ttffile' => "@".$_FILES['font_file']['tmp_name'],
+			'api_key' => $uaf_api_key
+		);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		$response = curl_exec($ch);
+		if(curl_errno($ch)) {
+			echo 'Error: ' . curl_error($ch);
+			exit();
+		}
+		else {
+			$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			if ($http_code == 200) {
+				$newFileName		= $fontNameToStore.'.woff';
+				$newFilePath		= $upload_dir['path'].'/'.$newFileName;
+				$fh = fopen($newFilePath, 'w') or die("Can't open file");
+				fwrite($fh, $response);
+				fclose($fh);
+			} else {
+				echo $response;
+				exit();
+			}
+		}
+		
+		//CONVERT TO EOT
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_VERBOSE, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: multipart/form-data"));
+		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux i686; rv:6.0) Gecko/20100101 Firefox/6.0Mozilla/4.0 (compatible;)");
+		curl_setopt($ch, CURLOPT_URL, 'http://dineshkarki.com.np/font-convertor/eot.php');
+		curl_setopt($ch, CURLOPT_POST, true);
+		$post = array(
+			'ttffile' => "@".$_FILES['font_file']['tmp_name'],
+			'api_key' => $uaf_api_key
+		);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		$response = curl_exec($ch);
+		if(curl_errno($ch)) {
+			echo 'Error: ' . curl_error($ch);
+			exit();
+		}
+		else {
+			$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			if ($http_code == 200) {
+				$newFileName		= $fontNameToStore.'.eot';
+				$newFilePath		= $upload_dir['path'].'/'.$newFileName;
+				$fh = fopen($newFilePath, 'w') or die("Can't open file");
+				fwrite($fh, $response);
+				fclose($fh);
+			} else {
+				echo $response;
+				exit();
+			}
+		}
+	//endif;
+	$fontsRawData 	= get_option('uaf_font_data');
+	$fontsData		= json_decode($fontsRawData, true);
+	if (empty($fontsData)):
+		$fontsData = array();
+	endif;
+	
+	$fontsData[date('ymdhis')]	= array('font_name' => $_POST['font_name'], 'font_path' => $fontNameToStoreWithUrl);
+	$updateFontData	= json_encode($fontsData);
+	update_option('uaf_font_data',$updateFontData);
+	uaf_write_css();
+	$show_msg 		= 'Font Added';
+}
+
+if ($_GET['delete_font_key']):
+	$fontsRawData 	= get_option('uaf_font_data');
+	$fontsData		= json_decode($fontsRawData, true);
+	$key_to_delete	= $_GET['delete_font_key'];
+	@unlink(realpath($fontsData[$key_to_delete]['font_path'].'.woff'));
+	@unlink(realpath($fontsData[$key_to_delete]['font_path'].'.eot'));
+	unset($fontsData[$key_to_delete]);
+	$updateFontData	= json_encode($fontsData);
+	update_option('uaf_font_data',$updateFontData);
+	$show_msg = 'Font Deleted';
+	uaf_write_css();
+endif;
+?>
+
+<table class="wp-list-table widefat fixed bookmarks">
+    <thead>
+        <tr>
+            <th>Upload Fonts</th>
+        </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>
+
+<?php if (!empty($show_msg)):?>
+	<div class="updated" id="message"><p><?php echo $show_msg ?></p></div>
+<?php endif; ?>
+
+<?php 
+$fontsRawData 	= get_option('uaf_font_data');
+$fontsData		= json_decode($fontsRawData, true);
+?>
+
+<p align="right"><input type="button" name="open_add_font" onClick="open_add_font();" class="button-primary" value="Add Fonts" /><br/></p>
+
+<div id="font-upload" style="display:none;">
+	<form action="options-general.php?page=use-any-font/plugin_interface.php" id="open_add_font_form" method="post" enctype="multipart/form-data">
+    	<table class="uaf_form">
+        	<tr>
+            	<td width="175">Font Name</td>
+                <td><input type="text" name="font_name" value="" class="required" style="width:200px;" /></td>
+            </tr>	
+            <tr>    
+                <td>Font File</td>
+                <td><input type="file" name="font_file" value="" class="required" /><br/>
+                <em>Only ttf format.</em>
+                </td>
+            </tr>
+            <tr>        
+                <td>&nbsp;
+                	
+                </td>
+                <td><input type="submit" name="submit-uaf-font" class="button-primary" value="Upload" />
+                <p>By clicking on Upload, you confirm that you have rights to use this font.</p>
+                </td>
+            </tr>
+        </table>	
+    </form>
+    <br/><br/>
+</div>
+
+<table cellspacing="0" class="wp-list-table widefat fixed bookmarks">
+	<thead>
+    	<tr>
+        	<th width="20">Sn</th>
+            <th>Font</th>
+            <th width="100">Delete</th>
+        </tr>
+    </thead>
+    
+    <tbody>
+    	<?php if (!empty($fontsData)): ?>
+        <?php 
+		$sn = 0;
+		foreach ($fontsData as $key=>$fontData):
+		$sn++
+		?>
+        <tr>
+        	<td><?php echo $sn; ?></td>
+            <td><?php echo $fontData['font_name'] ?></td>
+            <td><a onclick="if (!confirm('Are you sure ?')){return false;}" href="options-general.php?page=use-any-font/plugin_interface.php&delete_font_key=<?php echo $key; ?>">Delete</a></td>
+        </tr>
+        <?php endforeach; ?>
+        <?php else: ?>
+        <tr>
+        	<td colspan="3">No font found. Please click on Add Fonts to add font</td>
+        </tr>
+        <?php endif; ?>        
+    </tbody>
+    
+</table>
+
+<script>
+	function open_add_font(){
+		jQuery('#font-upload').toggle('fast');
+		jQuery("#open_add_font_form").validate({
+		  rules: {
+			font_name 			: {required:true, maxlength:40},
+			font_file 			: {required:true}
+			//accept:"ttf|otf|woff"
+			},
+		  messages:{
+			//font_file			: {accept:'Only accepts ttf,otf,woff'}
+		  }
+		});
+	}	
+</script>
+<br/>
+</td>
+</tr>
+</tbody>
+</table><br/>
---
+++ includes/uaf_footer.php
@@ -0,0 +1,86 @@
+        <br/>
+        <table class="wp-list-table widefat fixed bookmarks">
+            	<thead>
+                <tr>
+                	<th>Instruction</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                	<td>
+                    	<ol>
+                        	<li>Get API key from <a href="http://dineshkarki.com.np/font-convertor/api/" target="_blank">here</a>. You can offer your contribution from (Free to $100) and get the API key. All API key comes with lifetime validity.<br/>
+                            <em><strong>Note:</strong> API key is needed to connect to our server for font conversion.</em> 
+                            </li>
+                            
+                            <li>Upload your font in ttf format from <strong>Upload Fonts</strong> section. The required font format will be converted automatically by the plugin and stores in your server.
+                            <em><strong>Note:</strong> We don't store any font in our server. We delete the temporary files after conversion has been done.</em> 
+                            </li>
+                            
+                            <li>Assign your font to you html elements from <strong>Assign Font</strong> section.</li>
+                            
+                            <li>You are ready now. If you still have any problem visit our <a href="http://dineshkarki.com.np/forums/forum/use-any-fonts" target="_blank">support forum</a> or you can write to us directly using our contact form.</li>
+                            
+                        </ol>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        
+        </td>
+        <td width="15">&nbsp;</td>
+        <td width="250" valign="top">
+        	<table class="wp-list-table widefat fixed bookmarks">
+            	<thead>
+                <tr>
+                	<th>Support</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                	<td>If you have any issues, click <a href="http://dineshkarki.com.np/forums/forum/use-any-fonts" target="_blank">here</a> to visit our support forum</td>
+                </tr>
+                </tbody>
+            </table>
+            <br/>
+            <table class="wp-list-table widefat fixed bookmarks">
+            	<thead>
+                <tr>
+                	<th>Plugins You May Like</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                	<td>
+                    	<ol>
+                        	<li><a href="http://wordpress.org/extend/plugins/any-mobile-theme-switcher/" target="_blank">Any Mobile Theme Switcher</a></li>
+                            <li><a href="http://wordpress.org/extend/plugins/add-tags-and-category-to-page/" target="_blank">Add Tags And Category To Page</a></li>
+                            <li><a href="http://wordpress.org/extend/plugins/block-specific-plugin-updates/" target="_blank">Block Specific Plugin Updates</a></li>
+                            <li><a href="http://wordpress.org/extend/plugins/featured-image-in-rss-feed/" target="_blank">Featured Image In RSS Feed</a></li>
+                            <li><a href="http://wordpress.org/extend/plugins/remove-admin-bar-for-client/" target="_blank">Remove Admin Bar</a></li>
+                            <li><a href="http://wordpress.org/extend/plugins/html-in-category-and-pages/" target="_blank">.html in category and page url</a></li>
+                        </ol>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <br/>
+            <table class="wp-list-table widefat fixed bookmarks">
+            	<thead>
+                <tr>
+                	<th>Facebook</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                	<td><iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FDnessCarKey%2F77553779916&amp;width=185&amp;height=258&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color=%23f9f9f9&amp;header=false&amp;appId=215419415167468" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:240px; height:258px;" allowTransparency="true"></iframe>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <br/>
+            
+        </td>
+    </tr>
+</table>
+</div>
